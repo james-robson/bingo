e85543943694c80b4364cb067bfde44b29cfff59
James Robson <jamesrobson87@gmail.com>
2018-02-16 12:56:50 +0000

    Added the drop shadow to the tickets

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Flex>
+        <Flex justify='flex-end'>
           <TicketContainer tickets={this.state.tickets} />
         </Flex>
       </div>
